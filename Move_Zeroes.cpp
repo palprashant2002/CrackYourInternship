7ab513ee818548fe170a7f5b28cffe99d0a7b94d
palprashant2002 <palprashant234@gmail.com>
2022-05-17 02:22:34 +0530

    fifth

---
+++ Move_Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0,j=0,temp;
+        for(int j=0;j<nums.size();j++)
+        {
+            if(nums[j]!=0)
+            {
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+        }
+    }
+};
